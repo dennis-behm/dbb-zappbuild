d9c8b8deab30bb4929df9a7b3c5b3a57e06f8c5c
Dennis Behm <dennis.behm@de.ibm.com>
2026-06-09 08:57:50 +0200

    Fix syntax
    Signed-off-by: Dennis Behm <dennis.behm@de.ibm.com>

--- utilities/BuildUtilities.groovy
+++ utilities/BuildUtilities.groovy
@@ -993,7 +993,7 @@ def loadBuildProperties(String propertyFile) {
 	File propFile = new File("$propertyFile")
 	
 	if (propFile.exists()) {
-		 if (propFile.contains(props.buildConf) || propFile.contains(props.applicationConfDir)) {
+		 if (propertyFile.contains(props.buildConf) || propertyFile.contains(props.applicationConfDir)) {
 		 	props.load(propFile)
 		 } else {
 			 println "*!* The specified $propertyFile is not loaded from either build-conf ${props.buildConf} or application-conf ${props.applicationConfDir} paths. Avoid external property file loading. Build exits."
